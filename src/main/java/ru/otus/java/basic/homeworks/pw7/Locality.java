332ab3902bb171a300b9356258a743a7467ff851
axt-ru <81510428+axt-ru@users.noreply.github.com>
2025-01-03 19:41:52 +0300

    Delete src/main/java/ru/otus/java/basic/homeworks/pw7 directory

--- src/main/java/ru/otus/java/basic/homeworks/pw7/Locality.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.java.basic.homeworks.pw7;
-
-public enum Locality {
-    DENSE_FOREST, // лес густой
-    PLAIN, // равнина
-    SWAMP  // болото
-}
--- src/main/java/ru/otus/java/basic/homeworks/pw7/Transport.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.java.basic.homeworks.pw7;
-
-public interface Transport {
-    boolean move(int distance, Locality locality);
-
-    String getName(); // наименование транспорта
-}

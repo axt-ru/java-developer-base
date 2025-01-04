7ca317f439851bb938782895e4bc089dabac83ab
axt-ru <axtrr@mail.ru>
2025-01-04 18:18:54 +0300

    Выполнил ДЗ8

---
+++ src/main/java/ru/otus/java/basic/homeworks/pw8/AppArrayDataException.java
@@ -0,0 +1,8 @@
+package ru.otus.java.basic.homeworks.pw8;
+
+public class AppArrayDataException extends Exception {
+    public AppArrayDataException(String message) {
+
+        super(message);
+    }
+}
---
+++ src/main/java/ru/otus/java/basic/homeworks/pw8/AppArraySizeException.java
@@ -0,0 +1,7 @@
+package ru.otus.java.basic.homeworks.pw8;
+
+public class AppArraySizeException extends Exception {
+    public AppArraySizeException(String message) {
+        super(message);
+    }
+}
